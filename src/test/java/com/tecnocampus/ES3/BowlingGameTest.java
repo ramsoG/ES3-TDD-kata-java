15cb7e3aa553d1d0bd22d2feccdb5f90a07b1cdb
rogerRamosFernandez <35465039+ramsoG@users.noreply.github.com>
2023-04-19 13:31:32 +0200

    commit34

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -33,6 +33,14 @@ public class BowlingGameTest {
         assertThat(g.score()).isEqualTo(16);
     }
 
+    @Test public void one_strike() {
+        g.roll(10); // strike
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        assertThat(g.score()).isEqualTo(24);
+    }
+
     private void rollMany(int rolls, int pins) {
         for (var i = 0; i < rolls; i += 1)
             g.roll(pins);
