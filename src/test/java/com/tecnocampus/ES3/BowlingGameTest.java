49852ce6d1503d3375e68cdbd4de01d653f56aa4
rogerRamosFernandez <35465039+ramsoG@users.noreply.github.com>
2023-04-19 12:50:49 +0200

    commit5

--- src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
+++ src/test/java/com/tecnocampus/ES3/BowlingGameTest.java
@@ -1,8 +1,15 @@
 package com.tecnocampus.ES3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+
+    private Game g;
+    @BeforeEach
+    public void setUp(){
+        g = new Game();
+    }
 
     @Test
     public void create_game() {
@@ -11,7 +18,6 @@ public class BowlingGameTest {
 
     @Test
     public void roll_a_ball(){
-        var g = new Game();
         g.roll(0);
     }
 }
