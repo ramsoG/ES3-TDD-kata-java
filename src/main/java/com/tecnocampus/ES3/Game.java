8ceb9a49759c3ca079d1ba1d80a04021f11a49c0
rogerRamosFernandez <35465039+ramsoG@users.noreply.github.com>
2023-04-19 13:18:19 +0200

    commit21

--- src/main/java/com/tecnocampus/ES3/Game.java
+++ src/main/java/com/tecnocampus/ES3/Game.java
@@ -3,6 +3,8 @@ package com.tecnocampus.ES3;
 public class Game {
 
     private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
     public void roll(int pins) {
         score += pins;
     }
