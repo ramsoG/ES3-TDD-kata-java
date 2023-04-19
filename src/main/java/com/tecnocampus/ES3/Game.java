2d80b67783df232f581d0ea17c7cfba7051982d9
rogerRamosFernandez <35465039+ramsoG@users.noreply.github.com>
2023-04-19 13:35:36 +0200

    commit39

--- src/main/java/com/tecnocampus/ES3/Game.java
+++ src/main/java/com/tecnocampus/ES3/Game.java
@@ -12,7 +12,7 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) { // strike
+            if (isStrike(frameIndex)) { // strike
                 score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
@@ -40,5 +40,9 @@ public class Game {
 
     private boolean isSpare(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 }
